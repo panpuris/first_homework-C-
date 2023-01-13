a2e078b52b943be6c35672cf3e4a18dccb294451
Vitalii Puris <purisvit@mail.ru>
2023-01-14 01:25:38 +0200

    homework of task_6 - done

--- task_6/Program.cs
+++ task_6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"Ваше число {number} четное! =) ");
+}
+else
+{
+    System.Console.WriteLine($"Не братец, число {number} нечетное так не пойдет =(");
+}
