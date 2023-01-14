d25846efad55c1889817eb386843cde3045daea4
Vitalii Puris <purisvit@mail.ru>
2023-01-14 02:02:28 +0200

    homework of task_8 - done

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= num)
+{
+
+    if (count % 2 == 0)
+    {
+
+        System.Console.WriteLine(count);
+
+    }
+    count += 1;
+
+}
