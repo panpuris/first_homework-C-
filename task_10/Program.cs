a2c09874050a5b746c97347cc71c93e282533213
Vitalii Puris <purisvit@mail.ru>
2023-01-17 21:26:51 +0200

    Add folder task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (99 < number && number < 1000)
+{
+    System.Console.WriteLine(number / 10 % 10);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели неверное число, попробуйте еще раз!");
+}
