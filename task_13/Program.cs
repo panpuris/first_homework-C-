71305f1504f60113cbef7a687f79048730df28b2
Vitalii Puris <purisvit@mail.ru>
2023-01-18 11:49:19 +0200

    Add folder task_13

---
+++ task_13/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (99 < num && num < 1000)
+{
+    System.Console.WriteLine(num % 10);
+}
+else if (num < 100)
+{
+    System.Console.WriteLine("Третьего цифры нет");
+}
+else if (1000 < num && num < 10000)
+{
+    System.Console.WriteLine(num % 100 / 10);
+}
+else if (10000 < num && num < 100000)
+{
+    System.Console.WriteLine(num % 1000 / 100);
+}
+else if (100000 < num && num < 1000000)
+{
+    System.Console.WriteLine(num % 10000 / 1000);
+}
+else if (1000000 < num && num < 10000000)
+{
+    System.Console.WriteLine(num % 100000 / 10000);
+} // и так далее
+
+// Вариант через массив
+
+System.Console.WriteLine("Напишите Ваше число");
+string numbers = (Console.ReadLine()); ;
+System.Console.WriteLine(numbers[2]);
