69b4c2f99883787f0106b5c27e48957350ada3be
Vitalii Puris <purisvit@mail.ru>
2023-01-21 12:57:54 +0200

    task_23 done

--- task_23/Program.cs
+++ task_23/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
