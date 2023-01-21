80ca4fc4a91bc7f4d95f90850ef37fd3b47ec937
Vitalii Puris <purisvit@mail.ru>
2023-01-21 11:59:53 +0200

    Task_19 done

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -3,3 +3,28 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+
+System.Console.WriteLine("Введите пятизначное число для проверки: ");
+string? paliNum = Console.ReadLine();
+if (paliNum.Length == 5)
+{
+    if (paliNum[0] == paliNum[4] || paliNum[1] == paliNum[3])
+    {
+        System.Console.WriteLine($"Число {paliNum} является палиндромом");
+    }
+
+    else
+    {
+        System.Console.WriteLine($"Число {paliNum} не является палиндромом");
+    }
+}
+if (paliNum.Length > 5 || paliNum.Length < 5)
+{
+    System.Console.WriteLine("Введите корректное число!!!");
+}
+
+// //На случай если нам будет необходимо распечатать цифру из строчного масива, необходимо пользоваться двойной конвертацией
+// string? text = "123456";
+// int num = Convert.ToInt32(Convert.ToString(text[4]));
+// System.Console.WriteLine(num);
