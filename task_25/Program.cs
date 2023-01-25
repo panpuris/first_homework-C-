7c47bbd66df34ae2f3208e443ba2424eeebfaa22
Vitalii Puris <purisvit@mail.ru>
2023-01-25 14:46:49 +0200

    Add new versions to task_25

--- task_25/Program.cs
+++ task_25/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+//Решение с методами и проверкой
 int Rank(int numberOne, int numberTwo)
 {
     int result = numberOne;
@@ -11,31 +13,60 @@ int Rank(int numberOne, int numberTwo)
     return result;
 }
 
+int InputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
 
+        System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз!");
+    }
+    return number;
+}
+
+int numbA = InputNumber ("Введите число A : ");
+int numbB = InputNumber ("Введите число B : ");
+
+System.Console.WriteLine(Rank(numbA,numbB));
+
+
+//Решение без методов с проверкой
 
 System.Console.WriteLine("Введите число A : ");
-int numA = Convert.ToInt32(Console.ReadLine());
+string? numA = Console.ReadLine();
+bool result1 = int.TryParse(numA, out var number1);
 
 System.Console.WriteLine("Введите число B : ");
-int numB = Convert.ToInt32(Console.ReadLine());
+string? numB = Console.ReadLine();
+bool result2 = int.TryParse(numB, out var number2);
 
-// double result = Math.Pow(numA,numB);
-// System.Console.WriteLine();
-// System.Console.WriteLine(result);
+if (result1 == true && result2==true)
+{
+    System.Console.WriteLine(Math.Pow(number1,number2));
+}
+else
+{
+    System.Console.WriteLine("Введите корректные числа!!!");
+}
 
-// через цикл for
-// int result = numA;
-// for (int i = 1; i<numB; i++)
-// {
-//     result=result*numA;
-// }
+// Решение без методов и без проверки
 
-//через цикл while
-// int i = 1;
-// while (i<numB)
-// {
-//     result = result*numA;
-//     i++;
-// }
-System.Console.WriteLine(Rank(numA,numB));
+System.Console.WriteLine("Введите число A : ");
+int numbersA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B : ");
+int numbersB = Convert.ToInt32(Console.ReadLine());
+
+int result = numbersA;
+for (int i = 1; i<numbersB; i++)
+{
+    result=result*numbersA;
+}
+System.Console.WriteLine(result);
 
