281ce1b601679ea1328f86b08d262ed57b3a9428
Vitalii Puris <purisvit@mail.ru>
2023-02-11 13:04:04 +0200

    Task 66 done

---
+++ task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNum (int m, int n)
+{
+    if (m==n)return m;
+    return n + SumNum(m,n-1);
+    
+}
+System.Console.WriteLine(SumNum(4,8));
